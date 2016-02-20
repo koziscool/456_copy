ed74fd9ce66dd4f7d62fb5fd6a76b315c9f793a1
koziscool <kozjob@yahoo.com>
2016-02-20 15:26:57 -0600

    more stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+
+  has_many  :playlists
+  has_many  :bookmarks
+
+  def name
+    first_name + " " + last_name
+  end
+
+  def name_and_count
+    "#{first_name} #{last_name}(#{playlists.count})"
+  end
+
 end
