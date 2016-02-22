0e59ce6bf8af05c288e29d2709b605382694dcbe
koziscool <kozjob@yahoo.com>
2016-02-22 00:32:58 -0600

    associations for followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,13 @@ class User < ActiveRecord::Base
 
   has_many  :playlists
   has_many  :bookmarks
+  
+  has_many :initiated_followings, class_name: "Following", foreign_key: :follower_id
+  has_many :followeds, through: :initiated_followings, source: :followed
+
+  has_many :received_followings, class_name: "Following", foreign_key: :followed_id
+  has_many :followers, through: :received_followings, source: :follower
+
 
   def name
     first_name + " " + last_name
