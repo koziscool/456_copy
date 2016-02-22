f8b3dc847b2b2fdb613d594836b967794dc85c6d
koziscool <kozjob@yahoo.com>
2016-02-21 23:37:20 -0600

    polymorphic paths, helper function

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+  def get_mark_show_path( mark )
+    if mark.bookmarkable_type == "Artist"
+      the_path = artist_path( Artist.find( mark.bookmarkable_id ) )
+    end
+    if mark.bookmarkable_type == "Playlist"
+      the_path = playlist_path( Playlist.find( mark.bookmarkable_id ) )
+    end
+    if mark.bookmarkable_type == "Song"
+      the_path = song_path( Song.find( mark.bookmarkable_id ) )
+    end
+    the_path
+  end
+
 end
